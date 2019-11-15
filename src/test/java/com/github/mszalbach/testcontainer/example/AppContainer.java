0f6ce005c449f62b26a474f702cca7aec0392d34
Marcel Szalbach <marcel.szalbach@googlemail.com>
2019-11-16 00:20:04 +0100

    - added an container for own image - added a test which waits till the app is healthy and checks the hello endpoint

---
+++ src/test/java/com/github/mszalbach/testcontainer/example/AppContainer.java
@@ -0,0 +1,21 @@
+package com.github.mszalbach.testcontainer.example;
+
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+class AppContainer extends GenericContainer<AppContainer> {
+
+    AppContainer() {
+        super(System.getProperty("image.name"));
+        addExposedPorts(8080);
+        waitingFor(Wait.forHttp("/actuator/health"));
+    }
+
+    String getBaseURL() {
+        return "http://" + this.getContainerIpAddress() + ":" + this.getMappedPort(8080);
+    }
+
+    String getURL(String path) {
+        return getBaseURL() + path;
+    }
+}
---
+++ src/test/java/com/github/mszalbach/testcontainer/example/ApplicationTest.java
@@ -0,0 +1,29 @@
+package com.github.mszalbach.testcontainer.example;
+
+import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.Network;
+import org.testcontainers.containers.RabbitMQContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+
+import static io.restassured.RestAssured.given;
+import static org.apache.http.HttpStatus.SC_OK;
+import static org.hamcrest.CoreMatchers.is;
+
+@Testcontainers
+class ApplicationTest {
+
+    private static Network network = Network.newNetwork();
+
+    @Container
+    private RabbitMQContainer rabbitMQContainer = new RabbitMQContainer().withNetwork(network).withNetworkAliases("rabbit");
+
+    @Container
+    private AppContainer app = new AppContainer().withEnv("SPRING_RABBITMQ_HOST", "rabbit").dependsOn(rabbitMQContainer).withNetwork(network);
+
+
+    @Test
+    void should_be_reachable() {
+        given().get(app.getURL("/hello")).then().statusCode(SC_OK).body(is("Hello"));
+    }
+}
